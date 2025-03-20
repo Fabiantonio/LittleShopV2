6e726518cfc71be375b52a95a27ed24482051989
Fabian Casas <fa.casas@duocuc.cl>
2025-03-20 02:20:52 -0300

    error vercel

--- build_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Instalar dependencias
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-
-# Colectar archivos estáticos
-python3 manage.py collectstatic --noinput --clear
