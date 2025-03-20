b89ff40f8029d1a1683acb732a79232ec6bedd11
Fabian Casas <fa.casas@duocuc.cl>
2025-03-20 15:40:58 -0300

    error vercel

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn LittleShop.wsgi:application
--- build_files.sh
+++ build_files.sh
@@ -4,7 +4,7 @@
 echo "Building static files..."
 
 # Install dependencies
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --noinput
