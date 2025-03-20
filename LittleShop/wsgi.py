8ede8e74b54acd01c41d8cb59b5503e7d14d1291
Fabian Casas <fa.casas@duocuc.cl>
2025-03-20 02:10:22 -0300

    supabase migrate

--- LittleShop/wsgi.py
+++ LittleShop/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LittleShop.settings')
 
 application = get_wsgi_application()
+
+# Para compatibilidad con Vercel
+app = application
