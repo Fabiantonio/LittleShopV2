cc5ffcf35999902de8e856d102b4403f4e2c813a
Fabian Casas <fa.casas@duocuc.cl>
2025-03-20 01:50:10 -0300

    supabase migrate

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Crear directorio para archivos estáticos
-# Usar la ruta específica de Python proporcionada por Vercel
-/opt/vercel/python3.9/bin/python -m pip install -r requirements.txt
-/opt/vercel/python3.9/bin/python manage.py collectstatic --noinput
+# Instalar dependencias
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+
+# Colectar archivos estáticos
+python manage.py collectstatic --noinput --clear
